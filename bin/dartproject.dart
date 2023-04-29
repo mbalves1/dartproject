69fcf0b3123a50a39a3c8f909f7a6e06a78d7417
Murilo <mbalves1@outlook.com>
2023-04-29 13:20:29 -0300

    function

--- bin/dartproject.dart
+++ bin/dartproject.dart
@@ -1,5 +1,11 @@
 import 'package:dartproject/dartproject.dart' as dartproject;
 
+final name = 'Murilo';
+var calculateFunction = dartproject.calculate(2, 7);
+
 void main(List<String> arguments) {
-  print('Hello world: ${dartproject.calculate()}!');
+  print('Name: ${name}!');
+  print('Hello world: ${calculateFunction}');
 }
+
+// 14:15 min
--- lib/dartproject.dart
+++ lib/dartproject.dart
@@ -1,3 +1,6 @@
-int calculate() {
-  return 6 * 7;
-}
+// int calculate(int n1, [int n2 = 5]) {
+//   return n1 * n2;
+// }
+
+int calculate(int n1, [int n2 = 5]) => n1 * n2;
+
